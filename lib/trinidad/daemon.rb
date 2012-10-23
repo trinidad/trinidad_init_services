9d2bfda549d47ce19c82356b3ed3a6e7dade8425
kares <self@kares.org>
2012-10-23 11:25:18 +0200

    add comments on how daemon methods are invoked

--- lib/trinidad/daemon.rb
+++ lib/trinidad/daemon.rb
@@ -1,5 +1,9 @@
-require 'rubygems'
-require 'trinidad'
+begin
+  require 'trinidad'
+rescue LoadError => e
+  require('rubygems') && retry
+  raise e
+end
 
 module Trinidad
   module Daemon
@@ -7,10 +11,15 @@ module Trinidad
     def init
     end
 
+    # checked from com.msp.jsvc.JRubyDaemon.init
+    # as Trinidad::Daemon#setup? to check whether
+    # daemon's init setup has been succesful ...
     def setup?
       true
     end
 
+    # called from com.msp.jsvc.JRubyDaemon.start
+    # as Trinidad::Daemon#start
     def start(args = ARGV)
       Trinidad::CommandLineParser.parse(args)
       Trinidad.configuration.trap = false
@@ -18,6 +27,8 @@ module Trinidad
       @server.start
     end
 
+    # called from com.msp.jsvc.JRubyDaemon.stop
+    # as Trinidad::Daemon#stop
     def stop
       @server.stop
     end
--- lib/trinidad_init_services/version.rb
+++ lib/trinidad_init_services/version.rb
@@ -1,5 +1,5 @@
 module Trinidad
   module InitServices
-    VERSION = '1.2.1'
+    VERSION = '1.2.2'
   end
 end
